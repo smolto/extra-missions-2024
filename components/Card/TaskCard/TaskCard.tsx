9ca90de641c8df36a99f59d06184ac95f4829141
Santiago Molto <santiagomoltomartinez@gmail.com>
2024-06-04 19:48:05 +0200

    Fix bg size for some image

--- components/Card/TaskCard/TaskCard.tsx
+++ components/Card/TaskCard/TaskCard.tsx
@@ -15,9 +15,13 @@ export const TaskCard = ({
   link = `/session/${slug}/task/post/${id}`
 }: CardType) => {
   const bgClass = !image?.includes('bowser') ? styles['white-bg'] : styles[''];
+ 
+  const isCover = backgroundImage === 'https://extra-missions.s3.eu-north-1.amazonaws.com/task-background/mario-items.jpg' ? 'cover'
+  : 'contain';
+  
   return (
     <Link href={link}>
-      <article className={`${styles["card-item"]} ${styles[type]}`} style={{backgroundImage: `url(${backgroundImage})`}}>
+      <article className={`${styles["card-item"]} ${styles[type]}`} style={{backgroundImage: `url(${backgroundImage})`, backgroundSize: isCover}}>
         <img className={`${styles["image"]} ${bgClass}`} src={image} alt={title} />
         <div>
           <h2 className={styles["title"]}>{title}</h2>
